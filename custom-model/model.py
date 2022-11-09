b8a5f257a1c75dec2b1dfa8f05e63d82098f1894
ShreehariVaasishta <shreehari9481@gmail.com>
2022-11-09 17:04:58 +0530

    Add custom model predictor

---
+++ custom-model/model.py
@@ -0,0 +1,72 @@
+# Copyright 2021 The KServe Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import kserve
+from torchvision import models, transforms
+from typing import Dict
+import torch
+from PIL import Image
+import base64
+import io
+
+
+class AlexNetModel(kserve.Model):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.name = name
+        self.load()
+        self.model = None
+        self.ready = False
+
+    def load(self):
+        print("------LOAD - hollllla folks-------")
+        self.model = models.alexnet(pretrained=True)
+        self.model.eval()
+        self.ready = True
+
+    def predict(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
+        print("------PREDICT - hollllla folks-------")
+        inputs = payload["instances"]
+
+        # Input follows the Tensorflow V1 HTTP API for binary values
+        # https://www.tensorflow.org/tfx/serving/api_rest#encoding_binary_values
+        data = inputs[0]["image"]["b64"]
+
+        raw_img_data = base64.b64decode(data)
+        input_image = Image.open(io.BytesIO(raw_img_data))
+
+        preprocess = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+        input_tensor = preprocess(input_image)
+        input_batch = input_tensor.unsqueeze(0)
+
+        output = self.model(input_batch)
+
+        torch.nn.functional.softmax(output, dim=1)
+
+        values, top_5 = torch.topk(output, 5)
+
+        return {"predictions": values.tolist()}
+
+
+if __name__ == "__main__":
+    model = AlexNetModel("custom-model")
+    model.load()
+    kserve.ModelServer(workers=1).start([model])
